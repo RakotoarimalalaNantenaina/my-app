e0e14e36db6e61697cb4588c4aa32bd31f86a1f9
RakotoarimalalaNantenaina <naynah344@gmail.com>
2019-03-08 17:58:18 +1000

    2 eme projet

--- src/App.js
+++ src/App.js
@@ -1,35 +1,64 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router,Route, Link } from 'react-router-dom'
-import Accueil from './component/accueil'
-import Apropos from './component/apropos'
-import Contact from './component/contact'
+import React, { useState } from 'react'
+import UserTable from './tables/UserTable'
 import './App.css';
 
+const App = () => {
+  const usersData = [
+    
+  ]
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-      <div className="App">
-        <header className="App-header">
-            <ul className="liste">
-              <li><Link to="/accueil">Accueil</Link></li>
-              <li><Link to="/apropos">A propos</Link></li>
-              <li><Link to="/contact">Contact</Link></li>
-            </ul>
+  const [users, setUsers] = useState(usersData)
+  
+  const addUser = user => {
+		user.id = users.length 
+		setUsers([ ...users, user ])
+	}
+	const deleteUser = id => {
+		setUsers(users.filter(user => user.id !== id))
+	}
 
-            <Route path="/" exact component={Accueil} />
-            <Route path="/accueil" component={Accueil} />
-            <Route path="/apropos" component={Apropos} />
-            <Route path="/contact" component={Contact} />
-        </header>
-      <footer>
-        <center>footer</center>
-      </footer>
+  return (
+    <div className="container">
+      <div className="flex-row">
+        <div className="flex-large">
+          <AddUserForm addUser={addUser} />
+        </div><br></br>
+        <div className="flex-large">
+          <UserTable users={users} deleteUser={deleteUser} />
+        </div>
       </div>
-      </Router>
-    );
-  }
+    </div>
+  )
 }
 
-export default App;
+const AddUserForm = props => {
+	const initialFormState = { id: null, name: '', username: '' }
+	const [ user, setUser ] = useState(initialFormState)
+
+	const handleInputChange = event => {
+		const { name, value } = event.target
+
+		setUser({ ...user, [name]: value })
+	}
+
+	return (
+		<form
+			onSubmit={event => {
+				event.preventDefault()
+				if (!user.name || !user.username) return
+
+				props.addUser(user)
+				setUser(initialFormState)
+			}}
+		>
+			<label id="nom">Nom &nbsp;&nbsp;</label>
+			<input type="text" name="name" value={user.name} onChange={handleInputChange} />
+			<label id="nom">&nbsp;&nbsp;Prénom &nbsp;&nbsp;</label>
+			<input type="text" name="username" value={user.username} onChange={handleInputChange} />&nbsp;&nbsp;
+			&nbsp;&nbsp;<button class="btn btn-primary">Ajouter</button>
+		</form>
+
+	)
+}	
+
+export default App
